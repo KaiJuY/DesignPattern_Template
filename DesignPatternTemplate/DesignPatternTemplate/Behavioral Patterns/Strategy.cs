7e04d5e76eb6e34c2e4f50bae36586250dca73fe
KaiJuY <sungodfever@gmail.com>
2023-09-15 23:40:47 +0800

    add strategy and builder

---
+++ DesignPatternTemplate/DesignPatternTemplate/Behavioral Patterns/Strategy.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTemplate
+{
+    //策略模式
+    //此Model主要是注重在行為上或方法上的
+    //假設有一種共同行為是所有類別都需要的
+    //但是可能會根據這個類別的不同又會有不同的變化
+    //跟工廠模式最大的差別是一個是注重在創立實例一個是注重在方法上
+    public abstract class CalculateAmount
+    {
+        public abstract void SetBill(int bill);
+        public abstract void doCalculate();
+    }
+    public class SilverMember : CalculateAmount
+    {
+        public SilverMember(int pay)
+        {
+            SetBill(pay);
+        }
+        private int Bill { get; set; }
+        public override void SetBill(int bill)
+        {
+            Bill = bill;
+        }
+        public override void doCalculate()
+        {
+            Console.WriteLine("Bill : {0}", Bill);
+        }
+    }
+    public class GoldMember : CalculateAmount
+    {
+        public GoldMember(int pay)
+        {
+            SetBill(pay);
+        }
+        private int Bill { get; set; }
+        public override void SetBill(int bill)
+        {
+            Bill = bill;
+        }
+        public override void doCalculate()
+        {
+            Console.WriteLine("Bill : {0}", Bill*0.9);
+        }
+    }
+    public class MasterMember : CalculateAmount
+    {
+        public MasterMember(int pay)
+        {
+            SetBill(pay);
+        }
+        private int Bill { get; set; }
+        public override void SetBill(int bill)
+        {
+            Bill = bill;
+        }
+        public override void doCalculate()
+        {
+            Console.WriteLine("Bill : {0}", Bill * 0.7);
+        }
+    }
+
+    public class CaculateManager
+    {
+        private CalculateAmount calculateAmount;
+        public CaculateManager(CalculateAmount calculateamount)
+        {
+            calculateAmount = calculateamount;
+        }
+        public void executeCaculate()
+        {
+            calculateAmount.doCalculate();
+        }
+    }
+
+    public class StrategyDemo
+    {
+        public void Demo()
+        {
+            CaculateManager member1 = new CaculateManager(new SilverMember(100));
+            CaculateManager member2 = new CaculateManager(new GoldMember(200));
+            CaculateManager member3 = new CaculateManager(new MasterMember(300));
+            member1.executeCaculate(); //100
+            member2.executeCaculate(); //180
+            member3.executeCaculate(); //210
+        }
+    }
+}
---
+++ DesignPatternTemplate/DesignPatternTemplate/Creational Patterns/Builder.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTemplate
+{
+    //建造者模式
+    //這個Model希望解決的問題是在物件創建時
+    //因為物件的屬性很多如果直接寫在建構子當參數傳入
+    //很容易出錯且不容易閱讀因此會在類別中新增一個Builder
+    //來協助創建時的參數傳入
+
+    public class Meal
+    {
+        private Meal(MealBuilder mealBuilder)
+        {
+            Price = mealBuilder.Price;
+            MainCourse = mealBuilder.MainCourse;
+            Dessert = mealBuilder.Dessert;
+            Drink = mealBuilder.Drink;
+        }
+        private double Price { get; set; }
+        private string MainCourse { get; set; }
+        private string Dessert { get; set; }
+        private string Drink { get; set; }
+        public double GetPrice() { return Price; }
+        public string GetMainCourse() { return MainCourse; }
+        public string GetDessert() { return Dessert; }
+        public string GetDrink() { return Drink; }
+        public class MealBuilder
+        {
+            public double Price { get; set; }
+            public string MainCourse { get; set; }
+            public string Dessert { get; set; }
+            public string Drink { get; set; }
+            public MealBuilder(string maincourse)
+            {
+                MainCourse = maincourse;
+            }
+            public MealBuilder SetMainCourse(string maincourse)
+            {
+                MainCourse = maincourse;
+                return this;
+            }
+            public MealBuilder SetDessert(string dessert)
+            {
+                Dessert = dessert;
+                return this;
+            }
+            public MealBuilder SetDrink(string drink)
+            {
+                Drink = drink;
+                return this;
+            }
+            public MealBuilder SetPrice(double price)
+            {
+                Price = price;
+                return this;
+            }
+            public Meal build()
+            {
+                return new Meal(this);
+            }
+        }
+    }
+    public class MealDemo
+    {
+        public void Demo()
+        {
+            Meal Beefmeal = new Meal.MealBuilder("Beef")
+                .SetDrink("Coke")
+                .SetDessert("IceCream")
+                .SetPrice(99.9)
+                .build();
+
+            Console.WriteLine("Meal Detail - \n Main Course : {0}\n Drink : {1}\n Dessert : {2}\n Price : {3}\n ", 
+                Beefmeal.GetMainCourse(), Beefmeal.GetDrink(), Beefmeal.GetDessert(), Beefmeal.GetPrice());
+            /*
+             * Meal Detail - 
+             * Main Course : Beef
+             * Drink : Coke
+             * Dessert : IceCream
+             * Price : 99.9
+             */
+        }
+    }
+}

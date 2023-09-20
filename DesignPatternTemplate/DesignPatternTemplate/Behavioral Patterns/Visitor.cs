2902c3553292a0b94755d4274f12bc138f349155
KaiJuY <sungodfever@gmail.com>
2023-09-20 23:15:26 +0800

    complete Visitor

--- DesignPatternTemplate/DesignPatternTemplate/Behavioral Patterns/Visitor.cs
+++ DesignPatternTemplate/DesignPatternTemplate/Behavioral Patterns/Visitor.cs
@@ -15,6 +15,94 @@ namespace DesignPatternTemplate
     //因為原本的物件可能有他的職責但轉化成別的格式可能不是他的職責
     //而且今天可能被要求轉JSON明天可能又變成XML
     //所以透過另一個class進行visit並將物件直接傳入
-    //再透過這個class去判斷是哪種物件調用不同的多載function
-    
+    //再透過這個class去判斷是哪種物件調用不同的overloading function
+    public interface Visitor
+    {
+        public void access(soundbar sb);
+        public void access(monitor mon);
+        public void access(keyboard kb);
+    }
+    public class ComputerVisitor : Visitor
+    {
+        public void access(soundbar sb)
+        {
+            Console.WriteLine("This SoundBar Brand : {0}, SoundTrack : {1}, PowerAMP : {2}", sb.brand, sb.soundtrack, sb.poweramp);
+        }
+        public void access(monitor mon)
+        {
+            Console.WriteLine("This Monitor Brand : {0}, DisplaySize : {1}, Update Frequency : {2}", mon.brand, mon.displaysize, mon.update_fre);
+        }
+        public void access(keyboard kb)
+        {
+            Console.WriteLine("This KeyBoard Brand : {0}, Switch Type : {1}, Wireless : {2}", kb.brand, kb.switchtype, kb.wireless);
+        }
+    }
+    public interface ComputerFittings
+    {
+        public void accept(ComputerVisitor cv);
+    }
+    public class soundbar : ComputerFittings
+    {
+        public soundbar(string b, double st, int pa)
+        {
+            brand = b;
+            soundtrack = st;
+            poweramp = pa;
+        }
+        public string brand {get; set;}
+        public double soundtrack {get; set;}
+        public int poweramp{get; set;}
+        public void accept(ComputerVisitor cv)
+        {
+            cv.access(this);
+        }
+
+    }
+    public class monitor : ComputerFittings
+    {
+        public monitor(string b, int ds, double uf)
+        {
+            brand = b;
+            displaysize = ds;
+            update_fre = uf;
+        }
+        public string brand {get; set;}
+        public int displaysize {get; set;}
+        public double update_fre {get; set;}
+        public void accept(ComputerVisitor cv)
+        {
+            cv.access(this);
+        }
+    }
+    public class keyboard : ComputerFittings
+    {
+        public keyboard(string b, string st, bool wl)
+        {
+            brand = b;
+            switchtype = st;
+            wireless = wl;
+        }
+        public string brand {get; set;}
+        public string switchtype {get; set;}
+        public bool wireless {get; set;}
+        public void accept(ComputerVisitor cv)
+        {
+            cv.access(this);
+        }
+    }
+    public class VisitorDemo
+    {
+        public void demo()
+        {
+            soundbar BOSE_Soundbar = new soundbar("BOSE", 5.1, 20);
+            monitor SAMSUNG_Monitor = new monitor("SAMSUNG", 32, 120);
+            keyboard RAZER_Keyboard = new keyboard("RAZER", "Green", true);
+            BOSE_Soundbar.accept(new ComputerVisitor());
+            //This SoundBar Brand : BOSE, SoundTrack : 5.1, PowerAMP : 20
+            SAMSUNG_Monitor.accept(new ComputerVisitor());
+            //This Monitor Brand : SAMSUNG, DisplaySize : 32, Update Frequency : 120
+            RAZER_Keyboard.accept(new ComputerVisitor());
+            //This KeyBoard Brand : RAZER, Switch Type : Green, Wireless : true
+        }
+    }
 }

7ade178b1665857c8c9a0f17bcb34e8671190ae6
KaiJuY <sungodfever@gmail.com>
2023-09-30 00:10:52 +0800

    Add Composite

---
+++ DesignPatternTemplate/DesignPatternTemplate/Structural Patterns/Composite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTemplate
+{
+    //組合模式
+    //此模式優點是初始化的類與其他的Group相同
+    //這樣就可以讓所有Group的都有相同的Field
+    //但有個缺點無法依賴反轉因為都是直接使用class
+    
+    
+}
